03a266f6b6b33c2b50377aae114a006165917b28
Mohammad Ranjbar <mohammadranjbar.mmr81@gmail.com>
2024-05-31 15:58:56 +0330

    Created ErrorResponse Class

---
+++ src/main/java/com/blog_application/exception/ErrorResponse.java
@@ -0,0 +1,18 @@
+package com.blog_application.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private int status;
+    private String message;
+    private long timestamp;
+
+}
--- src/main/java/com/blog_application/exception/GlobalExceptionHandler.java
+++ src/main/java/com/blog_application/exception/GlobalExceptionHandler.java
@@ -1,8 +1,8 @@
 package com.blog_application.exception;
 
-import com.blog_application.util.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,8 +10,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ApiResponse> resourceNotfoundExceptionHandler(ResourceNotFoundException resourceNotFoundException){
-        return new ResponseEntity<>(new ApiResponse(resourceNotFoundException.getMessage(),false), HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorResponse> resourceNotfoundExceptionHandler(ResourceNotFoundException resourceNotFoundException){
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                resourceNotFoundException.getMessage(),
+                System.currentTimeMillis()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
 }
