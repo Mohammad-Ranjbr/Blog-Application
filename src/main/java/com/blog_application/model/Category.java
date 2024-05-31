196ed7c5e9b47d4257785b00323df5c08399e4cd
Mohammad Ranjbar <mohammadranjbar.mmr81@gmail.com>
2024-05-31 23:59:51 +0330

    Created Category entity class and CategoryDto data transfer object

---
+++ src/main/java/com/blog_application/model/Category.java
@@ -0,0 +1,23 @@
+package com.blog_application.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+
+}
