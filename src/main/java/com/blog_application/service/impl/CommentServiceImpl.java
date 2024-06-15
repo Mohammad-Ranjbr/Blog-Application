10287d196f8b882c23c8691b92d8fd7783f2492f
Mohammad Ranjbar <mohammadranjbar.mmr81@gmail.com>
2024-06-16 00:25:03 +0330

    add logging to CommentServiceImpl class

--- src/main/java/com/blog_application/service/impl/CommentServiceImpl.java
+++ src/main/java/com/blog_application/service/impl/CommentServiceImpl.java
@@ -11,43 +11,53 @@ import com.blog_application.repository.CommentRepository;
 import com.blog_application.service.CommentService;
 import com.blog_application.service.PostService;
 import com.blog_application.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CommentServiceImpl implements CommentService {
 
-    private final UserService userService;
-    private final CommentMapper commentMapper;
-    private final PostService postService;
     private final PostMapper postMapper;
+    private final UserService userService;
+    private final PostService postService;
+    private final CommentMapper commentMapper;
     private final CommentRepository commentRepository;
+    private final static Logger logger = LoggerFactory.getLogger(CommentServiceImpl.class);
 
     @Autowired
     public CommentServiceImpl(CommentMapper commentMapper,PostService postService,
                               CommentRepository commentRepository,PostMapper postMapper,UserService userService){
-        this.userService = userService;
-        this.commentMapper = commentMapper;
         this.postMapper = postMapper;
+        this.userService = userService;
         this.postService = postService;
+        this.commentMapper = commentMapper;
         this.commentRepository = commentRepository;
     }
 
     @Override
     public CommentDto createComment(CommentDto commentDto, Long post_id,Long user_id) {
+        logger.info("Creating comment : {}",commentDto.getContent());
         Post post = postMapper.toEntity(postService.getPostById(post_id));
         User user = userService.userDtoToUser(userService.getUserById(user_id));
         Comment comment = commentMapper.toEntity(commentDto);
         comment.setPost(post);
         comment.setUser(user);
         Comment savedComment = commentRepository.save(comment);
+        logger.info("Comment created successfully : {}",commentDto.getContent());
         return commentMapper.toDto(savedComment);
     }
 
     @Override
     public void deleteComment(Long comment_id) {
-        Comment comment = commentRepository.findById(comment_id).orElseThrow(() -> new ResourceNotFoundException("Comment","ID",String.valueOf(comment_id),"Get Comment not performed"));
+        logger.info("Deleting comment with ID : {}",comment_id);
+        Comment comment = commentRepository.findById(comment_id).orElseThrow(() -> {
+            logger.warn("Comment with ID {} not found, get comment not performed",comment_id);
+            return new ResourceNotFoundException("Comment","ID",String.valueOf(comment_id),"Get Comment not performed");
+        });
         commentRepository.delete(comment);
+        logger.info("Comment with ID : {} deleted successfully",comment_id);
     }
 
 }
