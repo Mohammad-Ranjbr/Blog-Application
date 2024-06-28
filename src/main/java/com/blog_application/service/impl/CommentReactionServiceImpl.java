8d0dd9c3be20d6c43deba31978e13606750c0b92
Mohammad Ranjbar <mohammadranjbar.mmr81@gmail.com>
2024-06-29 00:10:12 +0330

    fixed like and dislike comment bug. when user react the comment in comment table post id convert to null beacuse commentGetDto has not post

--- src/main/java/com/blog_application/service/impl/CommentReactionServiceImpl.java
+++ src/main/java/com/blog_application/service/impl/CommentReactionServiceImpl.java
@@ -4,12 +4,12 @@ import com.blog_application.config.mapper.CommentMapper;
 import com.blog_application.config.mapper.UserMapper;
 import com.blog_application.dto.comment.CommentGetDto;
 import com.blog_application.dto.comment.reaction.CommentReactionRequestDTO;
+import com.blog_application.exception.ResourceNotFoundException;
 import com.blog_application.model.Comment;
 import com.blog_application.model.CommentReaction;
 import com.blog_application.model.User;
 import com.blog_application.repository.CommentReactionRepository;
 import com.blog_application.repository.CommentRepository;
-import com.blog_application.service.CommentService;
 import com.blog_application.service.CommentReactionService;
 import com.blog_application.service.UserService;
 import org.slf4j.Logger;
@@ -25,19 +25,17 @@ public class CommentReactionServiceImpl implements CommentReactionService {
     private final UserMapper userMapper;
     private final UserService userService;
     private final CommentMapper commentMapper;
-    private final CommentService commentService;
     private final CommentRepository commentRepository;
     private final CommentReactionRepository commentReactionRepository;
 
     private static final Logger logger = LoggerFactory.getLogger(CommentReactionServiceImpl.class);
 
     @Autowired
-    public CommentReactionServiceImpl(UserService userService, CommentService commentService, CommentReactionRepository commentReactionRepository,
-                                      UserMapper userMapper, CommentMapper commentMapper, CommentRepository commentRepository){
+    public CommentReactionServiceImpl(UserService userService, CommentReactionRepository commentReactionRepository,
+                                      UserMapper userMapper, CommentRepository commentRepository, CommentMapper commentMapper){
         this.userMapper = userMapper;
         this.userService  =userService;
         this.commentMapper = commentMapper;
-        this.commentService = commentService;
         this.commentRepository = commentRepository;
         this.commentReactionRepository = commentReactionRepository;
     }
@@ -45,7 +43,10 @@ public class CommentReactionServiceImpl implements CommentReactionService {
     public CommentGetDto likeDislikeComment(CommentReactionRequestDTO requestDTO) {
         logger.info("Starting likeDislikeComment...");
         User user = userMapper.toEntity(userService.getUserById(requestDTO.getUserId()));
-        Comment comment = commentMapper.toEntity(commentService.getCommentById(requestDTO.getCommentId()));
+        Comment comment = commentRepository.findById(requestDTO.getCommentId()).orElseThrow(() -> {
+            logger.warn("Comment with ID {} not found, Get comment operation not performed",requestDTO.getCommentId());
+            return new ResourceNotFoundException("Comment","ID",String.valueOf(requestDTO.getCommentId()),"Get Comment operation not performed");
+        });
         Optional<CommentReaction> existing  = commentReactionRepository.findByUserAndComment(user,comment);
 
         CommentReaction commentReaction;
