82d033a1725e2de7df8e6c9d18686d69848b0c7d
Mohammad Ranjbar <mohammadranjbar.mmr81@gmail.com>
2024-06-28 19:03:10 +0330

    Create LikeDislike entity and establish relationships with Comment and User entities

---
+++ src/main/java/com/blog_application/model/LikeDislike.java
@@ -0,0 +1,23 @@
+package com.blog_application.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "like_dislike")
+public class LikeDislike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private User user;
+    @ManyToOne
+    private Comment comment;
+    private boolean isLike;
+}
--- src/main/java/com/blog_application/model/User.java
+++ src/main/java/com/blog_application/model/User.java
@@ -47,6 +47,8 @@ public class User {
     private LocalDateTime createdAt;
     @UpdateTimestamp
     private LocalDateTime updatedAt;
+    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+    private List<LikeDislike> likeDislikes;
 
     //mappedBy="user" -> It means that the Foreign Key column is located in the Post table, which points to the User table.
     //cascade = CascadeType.ALL -> This property specifies that various operations (such as saving, updating, deleting, etc.)
