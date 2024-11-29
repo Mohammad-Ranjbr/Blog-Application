9df330acabaa288eb55f4b5abbc0f51d23ca2c47
Mohammad Ranjbar <mohammadranjbar.mmr81@gmail.com>
2024-11-30 00:55:25 +0330

    add AuthenticationEvents to handle AuthenticationSuccessEvent and AbstractAuthenticationFailureEvent

---
+++ src/main/java/com/blog_application/events/AuthenticationEvents.java
@@ -0,0 +1,24 @@
+package com.blog_application.events;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.AbstractAuthenticationFailureEvent;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class g {
+
+    @EventListener
+    public void onSuccess(AuthenticationSuccessEvent successEvent){
+        log.info("Login successful for the user : {}", successEvent.getAuthentication().getName());
+    }
+
+    @EventListener
+    public void onFaliure(AbstractAuthenticationFailureEvent failureEvent){
+        log.warn("Login failed for the user : {} due to : {}", failureEvent.getAuthentication().getName(),
+                failureEvent.getException().getMessage());
+    }
+
+}
